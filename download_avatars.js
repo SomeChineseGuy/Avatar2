821ef915c3e85580df94c1e3e04d9d9c3531b565
vagrant <alvin.cl.ng@gmail.com>
2017-04-11 04:33:39 +0000

    Finally Done!

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,14 @@
 const request = require('request');
+const fs = require('fs');
 const GITHUB_USER = "SomeChineseGuy";
 const GITHUB_TOKEN = "0b19111919ba897de6aac7f00e9fad8b778a64f4";
+const repoOwner = process.argv[2];
+const repoName = process.argv[3];
 
+console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+function getRepoContributors(owner, name, cb) {
+  let requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   let options = {
     url: requestURL,
     headers: {
@@ -14,7 +18,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
   let space = "";
   request.get(options)
     .on('error', (err) =>{
-      cb;
+      console.log("fucked 2");
     })
     .on('response', (response) =>{
       console.log(`Response code: ${response.statusCode}\n`);
@@ -27,15 +31,32 @@ function getRepoContributors(repoOwner, repoName, cb) {
     .on('end', (end) => {
       let final = JSON.parse(space);
       cb(final);
+      console.log(final);
     });
 }
 
-getRepoContributors("jquery", "jquery", (results) => {
-  for(i in results) {
-    console.log(results[i].avatar_url);
+getRepoContributors(repoOwner, repoName, (result) => {
+  for(var one of result) {
+    console.log(one.login + "   :   " + one.avatar_url);
+    const origFileName = one.login;
+    const newFileName = `./${origFileName}.jpg`;
+    downloadImageByURL(one.avatar_url, newFileName);
+    console.log(newFileName);
   }
 });
 
-function downloadImageByURL(url, filepath) {
-  
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+  .on('error', function (err) {
+    throw err;
+  })
+  .on('response', function (response) {
+    console.log('Response Status Code: ', response.statusCode);
+  })
+  .on('end', (end) => {
+    console.log("You're done!");
+  })
+  .pipe(fs.createWriteStream(filePath));
 }
+
+// downloadImageByURL()
